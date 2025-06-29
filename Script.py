324d61b4f14ee3e0bbe4d97010db9124146b75f9
jazin-lara <jaxallbots00@gmail.com>
2025-06-29 10:13:03 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -246,6 +246,8 @@ Loki S01 or Loki S01E04 or Lucifer S03E24</pre>
 
     CAPTION = """<b>📂 ғɪʟᴇɴᴀᴍᴇ : {file_name}
 
+{file_size}
+
 ➲ <a href=
 https://t.me/+88N9oM5WQfI2NmJl> FRIDAY MOVIE REQUEST GROUP </a>
 ➲ <a href=https://t.me/Friday_Filim_House
