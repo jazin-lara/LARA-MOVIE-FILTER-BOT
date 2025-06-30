f54ecbf3c5c403f799283e5f1a40278b59eac33d
Muhammed Aflah <aflahtbm@gmail.com>
2025-06-30 21:40:14 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,8 +1,15 @@
-import re
+import re
 from os import environ
 from Script import script 
 
 id_pattern = re.compile(r'^.\d+$')
+def is_enabled(value, default):
+    if value.lower() in ["true", "yes", "1", "enable", "y"]:
+        return True
+    elif value.lower() in ["false", "no", "0", "disable", "n"]:
+        return False
+    else:
+        return default
 
 # Bot information
 SESSION = environ.get('SESSION', 'TechVJBot')
