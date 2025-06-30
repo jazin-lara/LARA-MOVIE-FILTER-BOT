ae3aa8fa909d40b195ecaca539baced8a2b88d2b
Muhammed Aflah <aflahtbm@gmail.com>
2025-06-30 21:50:48 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -66,7 +66,7 @@ async def start(client, message):
         if CLONE_MODE == True:
             buttons.append([InlineKeyboardButton('ᴄʀᴇᴀᴛᴇ ᴏᴡɴ ᴄʟᴏɴᴇ ʙᴏᴛ', callback_data='clone')])
         reply_markup = InlineKeyboardMarkup(buttons)
-        m=await message.reply_sticker("") 
+        m=await message.reply_sticker("CAACAgIAAxkBAAEO0kNoYrkoG5Jptd5WXH4OE4VVWApeWQAC_BoAAnZlKUoLTrdWaksCPjYE") 
         await asyncio.sleep(1)
         await m.delete()
         await message.reply_photo(
