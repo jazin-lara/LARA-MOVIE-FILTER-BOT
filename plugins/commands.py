207aa3268424a7cf567307e218c7ede6ce4277ab
jazin-lara <jaxallbots00@gmail.com>
2025-07-05 09:30:37 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -67,9 +67,7 @@ async def start(client, message):
         if CLONE_MODE == True:
             buttons.append([InlineKeyboardButton('ᴄʀᴇᴀᴛᴇ ᴏᴡɴ ᴄʟᴏɴᴇ ʙᴏᴛ', callback_data='clone')])
         reply_markup = InlineKeyboardMarkup(buttons)
-        m=await message.reply_sticker("CAACAgIAAxkBAAEO0kNoYrkoG5Jptd5WXH4OE4VVWApeWQAC_BoAAnZlKUoLTrdWaksCPjYE") 
-        await asyncio.sleep(1)
-        await m.delete()
+        
         await message.reply_photo(
             photo=random.choice(PICS),
             caption=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
