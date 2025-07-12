98a4f439255e44bdbad48be839718c3862d0acaa
jazin-lara <jaxallbots00@gmail.com>
2025-07-12 21:58:43 +0530

    Update pm_filter.py

--- CloneTechVJ/pm_filter.py
+++ CloneTechVJ/pm_filter.py
@@ -41,7 +41,7 @@ async def pm_text(bot, message):
     user_id = message.from_user.id
     if content.startswith("/") or content.startswith("#"): return  # ignore commands and hashtags
     ai_search = True
-    reply_msg = await bot.send_message(message.from_user.id, f"<b><i>Searching For {content} 🔍</i></b>", reply_to_message_id=message.id)
+    reply_msg = await bot.send_message(message.from_user.id, f"<b><i>Searching For</i></b>", reply_to_message_id=message.id)
     await auto_filter(bot, content, message, reply_msg, ai_search)
     
 @Client.on_callback_query(filters.regex(r"^next"))
